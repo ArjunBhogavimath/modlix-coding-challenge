ae05b01c48d776a67d6dcac83760aa99b4bf8fbc
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-03-28 14:01:25 +0530

    added 1M search initial code

--- PeekingInsidePi/src/PiSearch1M.java
+++ PeekingInsidePi/src/PiSearch1M.java
@@ -1,2 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class PiSearch1M {
+
+    //load pi as string from file
+    public static String loadPiFromFile(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    public static int search(String sequence) {
+        try {
+            String piDigits = loadPiFromFile("data/pi_1million.txt");
+            return piDigits.indexOf(sequence);
+        } catch (IOException e) {
+            System.out.println("Error loading Pi file: " + e.getMessage());
+            return -1;
+        }
+    }
 }
